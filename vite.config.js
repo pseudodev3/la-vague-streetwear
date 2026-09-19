0f6bb544c7a320cac0daec7ccc7d4391637b000e
ghostt <63957220+pseudodev3@users.noreply.github.com>
2026-09-19 21:05:13 +0100

    Stop publishing production source maps

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: true,
+    sourcemap: false,
     minify: 'terser',
     rollupOptions: {
       input: {
